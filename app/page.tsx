79d845be874be43854ae5e339ddd4aba2d7886ce
Roselle <roselleroxtabuena@gmail.com>
2026-01-27 00:39:55 +0800

    fix link redirection

--- app/page.tsx
+++ app/page.tsx
@@ -123,14 +123,13 @@ export default function Personal() {
                 <ProjectVideo src={project.video} />
               </div>
               <div className="px-1">
-                <a
+                <Link
                   className="font-base group relative inline-block font-[450] text-zinc-900 dark:text-zinc-50"
                   href={project.link}
-                  target="_blank"
                 >
                   {project.name}
                   <span className="absolute bottom-0.5 left-0 block h-[1px] w-full max-w-0 bg-zinc-900 dark:bg-zinc-50 transition-all duration-200 group-hover:max-w-full"></span>
-                </a>
+                </Link>
                 <p className="text-base text-zinc-600 dark:text-zinc-400">
                   {project.description}
                 </p>
